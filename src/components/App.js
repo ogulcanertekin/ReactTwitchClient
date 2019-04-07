c5319de715e93c1860fcef3a085a87daeb9be2f4
ogulcanertekin <ogulcanertekinn@gmail.com>
2019-04-07 06:47:52 +0300

    Stream Components Scaffolding & Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,22 @@
 import React from 'react';
 import {BrowserRouter, Route,Link} from 'react-router-dom';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamDelete from './streams/StreamDelete';
+import StreamShow from './streams/StreamShow';
+import StreamCreate from './streams/StreamCreate';
 
-const PageOne = () => {             // Klasik a(anchor) etiketi kullanmak yerine Link kullanmak, Single Page Applerde sürekli istek yapılmasını önler.Tek bir index.html serve ettigimizden aslında kullanıcı farklı sayfaya gittigini düşünse dahi farklı bir istekte bulunmuyoruz.Yalnızca urli bir componenta baglayıp gösteriyoruz. Ve uygulama içerisindeki state datalarımızın da tekrar ziyaret edildiginde kaybolmamasını saglıyor.Anchor taglar kullanılınca verilere erişimi kaybediyoruz. 
-    return(
-        <div>
-            PageOne
-            <br/>
-            <Link to="/pagetwo">Navigate to Page Two</Link>
-        </div>
-    ) 
-};
 
-const PageTwo = () => {
-    return(
-        <div>
-            PageTwo
-            <br/>
-            <Link to="/">Navigate to Page One</Link>
-        </div>
-    ) 
-};
-
-const App = () => {                             // Routers --> Include -->/pagetwo includes / so --> <Route path="/" component={PageOne} /> without exact keyword ->this display in /pagetwo... cause its include /
+const App = () => {                                             //Client-Side Routing!                         
     return (                
         <div>
             <BrowserRouter>
                 <div>
-                    <Route path="/" exact component={PageOne} />               
-                    <Route path="/pagetwo" component={PageTwo}/>
+                    <Route path="/" exact component={StreamList} />               
+                    <Route path="/streams/new" exact component={StreamCreate}/>
+                    <Route path="/streams/edit" exact component={StreamEdit}/>
+                    <Route path="/streams/delete" exact component={StreamDelete}/>
+                    <Route path="/streams/show" exact component={StreamShow}/>
                 </div>
             </BrowserRouter>
         </div>
