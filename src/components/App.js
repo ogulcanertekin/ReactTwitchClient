88f39c3de9dcd41963f31cbe2ab3d780c61823c5
ogulcanertekin <ogulcanertekinn@gmail.com>
2019-04-07 07:08:42 +0300

    Always Visible Components-Like Layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import {BrowserRouter, Route,Link} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import StreamEdit from './streams/StreamEdit';
 import StreamList from './streams/StreamList';
 import StreamDelete from './streams/StreamDelete';
 import StreamShow from './streams/StreamShow';
 import StreamCreate from './streams/StreamCreate';
-
+import Header from './Header';
 
 const App = () => {                                             //Client-Side Routing!                         
     return (                
-        <div>
+        <div className="ui container">               
             <BrowserRouter>
                 <div>
+                    <Header/>                                       {/* Always Visible Component Like Layout */}
                     <Route path="/" exact component={StreamList} />               
                     <Route path="/streams/new" exact component={StreamCreate}/>
                     <Route path="/streams/edit" exact component={StreamEdit}/>
