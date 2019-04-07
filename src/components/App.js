fc18f391983bbe9ffdcddba0b3ab9d169c00a47c
ogulcanertekin <ogulcanertekinn@gmail.com>
2019-04-07 05:44:47 +0300

    Navigating with Link in react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route,Link} from 'react-router-dom';
 
-const PageOne = () => {
-    return <div>PageOne</div>
+const PageOne = () => {             // Klasik a(anchor) etiketi kullanmak yerine Link kullanmak, Single Page Applerde sürekli istek yapılmasını önler.Tek bir index.html serve ettigimizden aslında kullanıcı farklı sayfaya gittigini düşünse dahi farklı bir istekte bulunmuyoruz.Yalnızca urli bir componenta baglayıp gösteriyoruz. Ve uygulama içerisindeki state datalarımızın da tekrar ziyaret edildiginde kaybolmamasını saglıyor.Anchor taglar kullanılınca verilere erişimi kaybediyoruz. 
+    return(
+        <div>
+            PageOne
+            <br/>
+            <Link to="/pagetwo">Navigate to Page Two</Link>
+        </div>
+    ) 
 };
 
 const PageTwo = () => {
     return(
         <div>
             PageTwo
-            <button>Click Me!</button>
+            <br/>
+            <Link to="/">Navigate to Page One</Link>
         </div>
     ) 
 };
