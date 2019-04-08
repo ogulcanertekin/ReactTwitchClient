61f1b9bd7e9055ea4b826e39a33d29ec38a5c308
ogulcanertekin <ogulcanertekinn@gmail.com>
2019-04-09 00:41:14 +0300

    Redux Forms-Connecting to redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux';
+import {reducer as formReducer} from 'redux-form';              // Given a name --> formReducer
 import authReducer from './authReducer';
 
 export default combineReducers({
-    auth:authReducer    
+    auth:authReducer,    
+    form:formReducer                                        //Dont need create any actions redux-form doing this automatically
 });
