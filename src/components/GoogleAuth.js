90cc44aa8d4ff7a05be4e0817922f544a86e7b77
ogulcanertekin <ogulcanertekinn@gmail.com>
2019-04-08 09:41:24 +0300

    Edit func names...

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -18,11 +18,11 @@ class GoogleAuth extends React.Component{           //Google Api - Gapi
         });
     }
 
-    onSignIn = () =>{
+    onSignInClick = () =>{
         this.auth.signIn();
     }
 
-    onSignOut = () =>{
+    onSignOutClick = () =>{
         this.auth.signOut();
     }
 
@@ -31,14 +31,14 @@ class GoogleAuth extends React.Component{           //Google Api - Gapi
             return null;
         }else if (this.state.isSignedIn){
             return (
-                <button onClick={this.onSignOut} className="ui red google button">
+                <button onClick={this.onSignOutClick} className="ui red google button">
                     <i className="google icon" />
                     Sign Out
                 </button>
             );
         }else{
             return (
-                <button onClick={this.onSignIn} className="ui red google button">
+                <button onClick={this.onSignInClick} className="ui red google button">
                     <i className="google icon" />
                     Sign In
                 </button>
