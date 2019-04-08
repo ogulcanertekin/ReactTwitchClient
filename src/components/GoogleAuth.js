f46fb24cc4f01f2c49307d7e83508bbc1ca6d72a
ogulcanertekin <ogulcanertekinn@gmail.com>
2019-04-08 09:11:10 +0300

    Authentication Status with GoogleApi methods

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,19 +1,43 @@
 import React from 'react';
 
-class GoogleAuth extends React.Component{
+class GoogleAuth extends React.Component{           //Google Api - Gapi
+
+    state = {isSignedIn:null};
+
     componentDidMount(){
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId:
                     '437310296749-fk16m3rael59jmp96m6ms59f6v6cr2ue.apps.googleusercontent.com',
                 scope:'email'
+            }).then(()=>{                                                       //Returns promise so using then right there!
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({isSignedIn:this.auth.isSignedIn.get()})          //isSignedIn - signIn - signOut --> built-in methods in gapi(GoogleApi)    
             });
         });
     }
 
+    renderAuthButton(){
+        if(this.state.isSignedIn===null){
+            return <div>I dont know if we are signed in</div>;
+        }else if (this.state.isSignedIn){
+            return <div>I am signed in</div>
+        }else{
+            return <div>I am not signed in</div>
+        }
+    }
+
     render(){
-        return <div>Google Auth</div>
+        return <div>{this.renderAuthButton()}</div>
     }
 }
+
+/*
+    Console-->
+
+    gapi.auth2.getAuthInstance()
+    gapi.auth2.getAuthInstance().signIn()
+    refresh ! 
+*/
 
 export default GoogleAuth;
