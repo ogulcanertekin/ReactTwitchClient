cedbc51c602c0b6d19eabf8db0eda085229f2a02
ogulcanertekin <ogulcanertekinn@gmail.com>
2019-04-09 09:34:19 +0300

    List of Records with _.mapKeys Lodash Function in streamReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import {reducer as formReducer} from 'redux-form';              // Given a name --> formReducer
 import authReducer from './authReducer';
+import streamReducer from './streamReducer';
 
 export default combineReducers({
     auth:authReducer,    
-    form:formReducer                                        //Dont need create any actions redux-form doing this automatically
+    form:formReducer,                                        //Dont need create any actions redux-form doing this automatically
+    streams:streamReducer
 });
--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -9,6 +9,8 @@ import {
 
 export default (state = {}, action) => {
     switch (action.type){
+        case FETCH_STREAMS:
+            return {...state, ..._.mapKeys(action.payload,'id')};       //Statedeki tüm Key Value seklinde olusturulmus verileri aldıktan sonra, Lodash mapKeys fonksiyonu array içerisindeki objelerin içinde bulunan ve 2. parametrede belirtilen propertye göre her bir objeye bir Key degeri atıyor...[1 :{id:1,name:hi},2:{id:2,name:hello}].Daha sonra da ... ile tüm bu verileri önceki  state datalarının üzerine ekler...Yani yeni eklenen bir  data varsa api istek yapıldıgında bunu anlayacak ve statee bunu ekleyerek güncelleyecek...Performans açısından da elverişli...
         case FETCH_STREAM:                                              //önceki state degelerine yeni bir Key->apidan gelen response un idsi olacak şekilde,value ise response data(action requestden gelen) ekliyoruz.
             return {...state,[action.payload.id]: action.payload};      //Object Based Approach-->Key-Value Interpolation ES6-->Inspected source code of react--> we cant mutation real state! just update and return new object..
         case CREATE_STREAM:
