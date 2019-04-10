1eba6efbb10509ab4248ad0f2e8840e664544b0b
ogulcanertekin <ogulcanertekinn@gmail.com>
2019-04-10 05:39:13 +0300

    Create Stream button if User Signed In & Link to /streams/new for post

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import {Link} from 'react-router-dom';
 import {fetchStreams} from '../../actions';
 
 class StreamList extends React.Component {
@@ -38,12 +39,25 @@ class StreamList extends React.Component {
         });
     }
 
+    renderCreate(){
+        if(this.props.isSignedIn){
+            return(
+                <div style={{textAlign:'right'}}>
+                    <Link to="/streams/new" className="ui button primary">
+                        Create Stream
+                    </Link>
+                </div>
+            );  
+        }
+    }
+
     render() {
         //console.log(this.props.streamz);
         return (
             <div>
                 <h2>Streams</h2>
                 <div className="ui celled list">{this.renderList()}</div>
+                {this.renderCreate()}
             </div>
         );
     }
@@ -52,7 +66,8 @@ class StreamList extends React.Component {
 const mapStateToProps = (state) => {                        
     return {
         streams: Object.values(state.streams),                   // Reducersda stateden bize gelen veri Object tipinde ancak biz burda propertyde bunu mapping ve listeleme işlemlerinin kolay olması için Arraya çevimek istiyoruz.Key olarak Id vermiştik bu objelere...Object.values fonksiyonu state içerisindeki key-value ciftlerinden value leri alarak bir diziye dönüştürür.[{title:"asda",name:"asdas"},{title:"qwewqe",name:"wqeqewqe"}]
-        currentUserId:state.auth.userId
+        currentUserId:state.auth.userId,
+        isSignedIn:state.auth.isSignedIn
     };          
 };
 
