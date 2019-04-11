4c96ea0a37cc4647c3d3609b711457cf58bad5d3
ogulcanertekin <ogulcanertekinn@gmail.com>
2019-04-11 07:03:09 +0300

    Fetching Single Data for Edit stream

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import {fetchStream} from '../../actions';
 
-const StreamEdit = (props) => {             //Router(react-router-dom) dan gelen default propertyler ile route parameters yakalanabilir.
-    //console.log(props.match.params.id);                      
-    return <div>StreamEdit</div>;
+class StreamEdit extends React.Component {           //Router(react-router-dom) dan gelen default propertyler ile route parameters yakalanabilir.
+
+    componentDidMount(){
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
+    render(){
+        if(!this.props.stream){
+            return <div>Loading...</div>
+        }
+        //console.log(this.props);                     
+        return <div>{this.props.stream.title}</div>;
+    } 
 };
 
 //mapStateToProps -->For Taking states from redux store...
@@ -15,4 +26,4 @@ const mapStateToProps = (state,ownProps) => {       //Component daki route prop 
     // -> streams stateleri -> streamList componentdidMount içerisinde fetch ediliyordu.->Ancak bu durumda anasayfaya ugramadan direk /streams/edit/3 dedigimizde akısı göremeyiz.React routerda component isolation yani --> fetch own data kuralı uygulanmalı...
 };
 
-export default connect(mapStateToProps)(StreamEdit);
+export default connect(mapStateToProps,{fetchStream})(StreamEdit);
